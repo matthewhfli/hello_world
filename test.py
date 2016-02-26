7d37ea87dfa7689055a436830fbffb8f4092c454
matthewhfli <matthewhfli@126.com>
2016-02-26 17:16:03 +0800

    hehehe

--- test.py
+++ test.py
@@ -1,10 +1,49 @@
 # coding=utf-8
+import re
 # import urladmin
 #
 # uset = urladmin.getallnoteurl()
 # for b in uset:
 #     print b
 
-import logging
-logging.basicConfig(format='%(asctime)s %(message)s', filename='info.log',level=logging.DEBUG)
-logging.info('good morning')
+# import logging
+# logging.basicConfig(format='%(asctime)s %(message)s', filename='info.log',level=logging.DEBUG)
+# logging.info('good morning')
+
+
+# phoneset = set()
+# filename = "yichen.txt"
+# with open(filename) as f:
+#     content = f.read().splitlines()
+#     for line in content:
+#         #print line
+#         l = re.findall('1[3|5|7|8|][0-9]{9}',line)
+#         for e in l:
+#             if e not in phoneset:
+#                 phoneset.add(e)
+#             print e
+# print phoneset.__len__()
+
+import urllib2
+import json
+opener = urllib2.build_opener()
+f = opener.open('https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=13762096719')
+s = '*'+f.read().decode('gbk')
+
+ss = s[s.index('{'):]
+print ss
+#ss = '{"hello":"world","good":"morning"}'
+#j = json.loads(ss)
+#print j
+
+# encodedjson = json.dumps(s)
+# print encodedjson
+
+
+# line = '手机：18349299386xyz18349299385'
+# m = re.findall('1[3|5|7|8|][0-9]{9}',line)
+# for e in m:
+#     print e
+
+
+#
