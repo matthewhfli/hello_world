9536a4a1d7d0d8dd0bbb06710b937895641234e2
matthewhfli <matthewhfli@126.com>
2016-03-01 14:13:16 +0800

    hehehe

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 import re
+import urllib2
+from BeautifulSoup import BeautifulSoup
 
 def extract_phone(line):
     info_list = line.split('<br/>')
@@ -33,9 +35,69 @@ def get_phone(line):
     return phone
 
 
-# filename = "yichen.txt"
-# with open(filename) as f:
-#     content = f.read().splitlines()
-#     for line in content:
-#         phone = get_phone(line)
-#         print phone
+def getboardurl():
+    urlhome = 'http://www.xx007.cn/'
+    urlboardset = set()
+    html = urllib2.urlopen(urlhome).read()
+    html = html.decode('gbk')
+    soup = BeautifulSoup(html)
+    boardlist = soup.findAll('a', attrs={'href': re.compile('^index\.asp\?boardid=.*')})
+
+    for board in boardlist:
+        urlboard = urlhome + board['href']
+        if urlboard not in urlboardset:
+            urlboardset.add(urlboard)
+    return urlboardset
+
+
+def getallnoteurl():
+    urlhome = 'http://www.xx007.cn/'
+    urlnoteset = set()
+    urlboardset = getboardurl()
+    for urlboard in urlboardset:
+        print urlboard
+        html = ""
+        soup = ""
+        try:
+            html = urllib2.urlopen(urlboard,timeout=30).read()
+            html = html.decode('gb18030')
+            soup = BeautifulSoup(html)
+        except:
+            continue
+
+        # 找出板块内所有帖子(note)的页数
+        tempstr = '';
+        tds = soup.findAll('td', attrs={'class': 'tabletitle1'})
+        for i in range(len(tds)):
+            if i == 2:
+                tempstr = tds[i].text
+        page = 0
+        try:
+            page = int(tempstr[tempstr.index("/") + 1:tempstr.index(u'页')])
+        except:
+            print "get page erro"
+            continue
+        print page
+
+        for i in range(1, page + 1):
+            if i > 3:
+                break
+            url = urlboard + "&action=&topicmode=0&page=" + str(i)
+            # print url
+            html=""
+            soup = ""
+            try:
+                html = urllib2.urlopen(url,timeout=30).read()
+                html = html.decode('gb18030')
+                soup = BeautifulSoup(html)
+            except:
+                print "soup erro2"
+                continue
+
+            listtitle = soup.findAll('div', attrs={'class': 'listtitle'})
+            for title in listtitle:
+                urlnote = urlhome + title.a["href"]
+                if urlnote not in urlnoteset:
+                    urlnoteset.add(urlnote)
+                    print urlnote
+    return urlnoteset
